d1241adf08530dd3c2af0f68f46772e332b9adb0
David Lougheed <david.lougheed@gmail.com>
2020-03-26 09:53:55 -0400

    Fix sqlite3 uri

--- chord_drs/config.py
+++ chord_drs/config.py
@@ -19,7 +19,7 @@ BASEDIR = os.environ.get("DATABASE", APP_DIR.parent)
 
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'sqlite://' + str(Path(os.path.join(BASEDIR, "db.sqlite3")).expanduser().resolve())
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + str(Path(os.path.join(BASEDIR, "db.sqlite3")).expanduser().resolve())
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     CHORD_URL: Optional[str] = os.environ.get("CHORD_URL", None)
     CHORD_SERVICE_URL_BASE_PATH: Optional[str] = os.environ.get("SERVICE_URL_BASE_PATH", None)
