9dc11361a4fa36ac4d4beac76ff4037a8576913c
David Lougheed <david.lougheed@gmail.com>
2023-01-12 16:22:31 -0500

    chore!: update DRS to use new service type and bento object in /service-info

--- chord_drs/constants.py
+++ chord_drs/constants.py
@@ -1,7 +1,17 @@
 from chord_drs import __version__
 
-__all__ = ["SERVICE_NAME", "SERVICE_ARTIFACT", "SERVICE_TYPE"]
+__all__ = [
+    "BENTO_SERVICE_KIND",
+    "SERVICE_NAME",
+    "SERVICE_ARTIFACT",
+    "SERVICE_TYPE",
+]
 
+BENTO_SERVICE_KIND = "drs"
 SERVICE_NAME = "Bento Data Repository Service"
-SERVICE_ARTIFACT = "drs"
-SERVICE_TYPE = f"ca.c3g.chord:{SERVICE_ARTIFACT}:{__version__}"
+SERVICE_ARTIFACT = BENTO_SERVICE_KIND
+SERVICE_TYPE = {
+    "group": "ca.c3g.chord",
+    "artifact": SERVICE_ARTIFACT,
+    "version": __version__,
+}
