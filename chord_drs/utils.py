411dcdf6d354cf219e59551a2cd3517ef1f2334e
David Lougheed <david.lougheed@gmail.com>
2023-02-07 14:59:39 -0500

    test file checksum directly + increase chunk size for checksumming

--- chord_drs/utils.py
+++ chord_drs/utils.py
@@ -3,12 +3,14 @@ from hashlib import sha256
 
 __all__ = ["drs_file_checksum"]
 
+CHUNK_SIZE = 16 * 1024
+
 
 def drs_file_checksum(path: str) -> str:
     hash_obj = sha256()
 
     with open(path, "rb") as f:
-        for chunk in iter(lambda: f.read(4096), b""):
+        while chunk := f.read(CHUNK_SIZE):
             hash_obj.update(chunk)
 
     return hash_obj.hexdigest()
