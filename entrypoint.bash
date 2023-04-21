c699f172e7d6d902d0c333678acfb2b1de944414
David Lougheed <david.lougheed@gmail.com>
2023-04-21 11:05:45 -0400

    fix: make data folders in entrypoint if DATABASE/DATA are specified

--- entrypoint.bash
+++ entrypoint.bash
@@ -10,16 +10,19 @@ chown -R bento_user:bento_user /drs
 if [[ -z "${BENTO_DRS_CONTAINER_DATA_VOLUME_DIR}" ]]; then
   # Location of volume mount - run chown here.
   chown -R bento_user:bento_user "${BENTO_DRS_CONTAINER_DATA_VOLUME_DIR}"
-else
-  # Otherwise, use the database and object folders more directly to set up paths & permissions.
-  if [[ -z "${DATABASE}" ]]; then
-    # DATABASE is a folder; confusing naming
-    chown -R bento_user:bento_user "${DATABASE}"
-  fi
-  if [[ -z "${DATA}" ]]; then
-    # DATA is another folder; confusing naming
-    chown -R bento_user:bento_user "${DATA}"
-  fi
+fi
+
+# Use the database and object folder path variables more directly to create them
+# and set their permissions.
+if [[ -z "${DATABASE}" ]]; then
+  # DATABASE is a folder; confusing naming
+  mkdir -p "${DATABASE}"
+  chown -R bento_user:bento_user "${DATABASE}"
+fi
+if [[ -z "${DATA}" ]]; then
+  # DATA is another folder; confusing naming
+  mkdir -p "${DATA}"
+  chown -R bento_user:bento_user "${DATA}"
 fi
 
 # Set .gitconfig for development, since we're overriding the base image entrypoint
