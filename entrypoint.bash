20f460c81a88710bd2b4d259c27c38398f29fb4c
Victor Rocheleau <victor.rocheleau@mcgill.ca>
2023-05-31 12:40:59 -0400

    fix dir creation conditions in entrypoint.bash

--- entrypoint.bash
+++ entrypoint.bash
@@ -7,27 +7,23 @@ source /create_service_user.bash
 
 # Fix permissions on /drs; fix data directory permissions more specifically if needed
 chown -R bento_user:bento_user /drs
-if [[ -z "${BENTO_DRS_CONTAINER_DATA_VOLUME_DIR}" ]]; then
+if [[ -n "${BENTO_DRS_CONTAINER_DATA_VOLUME_DIR}" ]]; then
   # Location of volume mount - run chown here.
   chown -R bento_user:bento_user "${BENTO_DRS_CONTAINER_DATA_VOLUME_DIR}"
 fi
 
 # Use the database and object folder path variables more directly to create them
 # and set their permissions.
-echo "${DATABASE}"
-if [[ -z "${DATABASE}" ]]; then
+if [[ -n "${DATABASE}" ]]; then
   # DATABASE is a folder; confusing naming
   mkdir -p "${DATABASE}"
   chown -R bento_user:bento_user "${DATABASE}"
-  echo "db dir created"
 fi
 
-echo "${DATA}"
-if [[ -z "${DATA}" ]]; then
+if [[ -n "${DATA}" ]]; then
   # DATA is another folder; confusing naming
   mkdir -p "${DATA}"
   chown -R bento_user:bento_user "${DATA}"
-  echo "data dir created"
 fi
 
 # Set .gitconfig for development, since we're overriding the base image entrypoint
