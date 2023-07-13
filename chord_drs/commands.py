6b8349917160cb1fd788050ed06528a65d290442
David Lougheed <david.lougheed@gmail.com>
2023-07-13 15:26:10 -0400

    feat: add permissions params to cli ingest script

--- chord_drs/commands.py
+++ chord_drs/commands.py
@@ -10,8 +10,16 @@ from .db import db
 from .models import DrsBlob, DrsBundle
 
 
-def create_drs_bundle(location: str, parent: DrsBundle | None = None) -> DrsBundle:
-    bundle = DrsBundle(name=os.path.basename(location))
+def create_drs_bundle(
+    location: str,
+    parent: DrsBundle | None = None,
+    project_id: str | None = None,
+    dataset_id: str | None = None,
+    data_type: str | None = None,
+) -> DrsBundle:
+    perms_kwargs = {"project_id": project_id, "dataset_id": dataset_id, "data_type": data_type}
+
+    bundle = DrsBundle(name=os.path.basename(location), **perms_kwargs)
 
     if parent:
         bundle.parent_bundle = parent
@@ -20,9 +28,9 @@ def create_drs_bundle(location: str, parent: DrsBundle | None = None) -> DrsBund
         f = os.path.abspath(os.path.join(location, f))
 
         if os.path.isfile(f):
-            create_drs_blob(f, parent=bundle)
+            create_drs_blob(f, parent=bundle, **perms_kwargs)
         else:
-            create_drs_bundle(f, parent=bundle)
+            create_drs_bundle(f, parent=bundle, **perms_kwargs)
 
     bundle.update_checksum_and_size()
     db.session.add(bundle)
@@ -32,8 +40,19 @@ def create_drs_bundle(location: str, parent: DrsBundle | None = None) -> DrsBund
     return bundle
 
 
-def create_drs_blob(location: str, parent: DrsBundle | None = None) -> None:
-    drs_blob = DrsBlob(location=location)
+def create_drs_blob(
+    location: str,
+    parent: DrsBundle | None = None,
+    project_id: str | None = None,
+    dataset_id: str | None = None,
+    data_type: str | None = None,
+) -> None:
+    drs_blob = DrsBlob(
+        location=location,
+        project_id=project_id,
+        dataset_id=dataset_id,
+        data_type=data_type,
+    )
 
     if parent:
         drs_blob.bundle = parent
@@ -45,8 +64,11 @@ def create_drs_blob(location: str, parent: DrsBundle | None = None) -> None:
 
 @click.command("ingest")
 @click.argument("source")
+@click.option("--project", default="", help="Project ID this object is attached to.")
+@click.option("--dataset", default="", help="Dataset ID this object is attached to.")
+@click.option("--data-type", default="", help="Data type this object is attached to.")
 @with_appcontext
-def ingest(source: str) -> None:
+def ingest(source: str, project: str, dataset: str, data_type: str) -> None:
     """
     When provided with a file or a directory, this command will add these
     to our list of objects, to be served by the application.
@@ -63,9 +85,11 @@ def ingest(source: str) -> None:
 
     source = os.path.abspath(source)
 
+    perms_kwargs = {"project_id": project or None, "dataset_id": dataset or None, "data_type": data_type or None}
+
     if os.path.isfile(source):
-        create_drs_blob(source)
+        create_drs_blob(source, **perms_kwargs)
     else:
-        create_drs_bundle(source)
+        create_drs_bundle(source, **perms_kwargs)
 
     db.session.commit()
