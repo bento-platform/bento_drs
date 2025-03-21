225c5731716203f7298876877b3c7706707c3b7c
v-rocheleau <victor.rocheleau@mcgill.ca>
2025-03-21 14:08:27 -0400

    chore: s3 mock server as pytest plugin

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,7 @@
-import signal
-import time
 from typing import Generator, Type, TypeVar
 import os
 import pathlib
 import pytest
-import requests
-import subprocess as sp
 import shutil
 
 from aioboto3 import Session
@@ -20,14 +16,10 @@ from unittest.mock import patch
 from chord_drs.backends.s3 import S3Backend
 from chord_drs.data_sources import DATA_SOURCE_LOCAL, DATA_SOURCE_S3
 
+from .constants import *
 
-AUTHZ_URL = "http://bento-authz.local"
-SQLALCHEMY_DATABASE_URI = "sqlite:///:memory:"
 
-DUMMY_PROJECT_ID = "b1738ea3-6ea7-4f43-a13f-51f4398818c4"
-DUMMY_DATASET_ID = "c96aa217-e07d-4d52-8c5c-df03f054fd3d"
-
-DATA_TYPE_PHENOPACKET = "phenopacket"
+T = TypeVar("T")
 
 
 def non_existant_dummy_file_path() -> str:  # Function rather than constant so we can set environ first
@@ -54,70 +46,13 @@ def empty_file_path():  # Function rather than constant so we can set environ fi
     return str(APP_DIR.parent / "tests" / "empty_file.txt")
 
 
-_proxy_bypass = {
-    "http": None,
-    "https": None,
-}
-
-
-def start_service(service_name, host, port):
-    moto_svr_path = shutil.which("moto_server")
-    args = [moto_svr_path, "-H", host, "-p", str(port)]
-    process = sp.Popen(args, stdin=sp.PIPE, stdout=sp.PIPE, stderr=sp.PIPE)  # shell=True
-    url = "http://{host}:{port}".format(host=host, port=port)
-
-    for i in range(0, 30):
-        output = process.poll()
-        if output is not None:
-            print("moto_server exited status {0}".format(output))
-            stdout, stderr = process.communicate()
-            print("moto_server stdout: {0}".format(stdout))
-            print("moto_server stderr: {0}".format(stderr))
-            pytest.fail("Can not start service: {}".format(service_name))
-
-        try:
-            # we need to bypass the proxies due to monkeypatches
-            requests.get(url, timeout=5, proxies=_proxy_bypass)
-            break
-        except requests.exceptions.ConnectionError:
-            time.sleep(0.5)
-    else:
-        stop_process(process)  # pytest.fail doesn't call stop_process
-        pytest.fail("Can not start service: {}".format(service_name))
-
-    return process
-
-
-def stop_process(process):
-    try:
-        process.send_signal(signal.SIGTERM)
-        process.communicate(timeout=20)
-    except sp.TimeoutExpired:
-        process.kill()
-        outs, errors = process.communicate(timeout=20)
-        exit_code = process.returncode
-        msg = "Child process finished {} not in clean way: {} {}".format(exit_code, outs, errors)
-        raise RuntimeError(msg)
-
-
-S3_HOST = "127.0.0.1"
-S3_PORT = "9000"
-S3_SERVER_URL = f"http://{S3_HOST}:{S3_PORT}"
-S3_ACCESS_KEY = "test_access_key"
-S3_SECRET_KEY = "test_secret_key"
-
-
-@pytest.fixture
-def s3_server():
-    process = start_service("s3", S3_HOST, S3_PORT)
-    yield S3_SERVER_URL
-    stop_process(process)
-
-
-T = TypeVar("T")
-
-
 def create_fake_session(base_class: Type[T], url_overrides: dict[str, str]) -> Type[T]:
+    """
+    Taken from aioboto3's unit tests: https://github.com/terricain/aioboto3/blob/main/tests/conftest.py
+
+    Creates a mocked session from the provided base class.
+    """
+
     class FakeSession(base_class):
         def __init__(self, *args, **kwargs):
             super(FakeSession, self).__init__(*args, **kwargs)
@@ -151,7 +86,12 @@ def create_fake_session(base_class: Type[T], url_overrides: dict[str, str]) -> T
 
 @pytest.fixture
 def s3_session(s3_server):
+    """
+    Taken from aioboto3's unit tests: https://github.com/terricain/aioboto3/blob/main/tests/conftest.py
 
+    Creates and starts a mocked aioboto3.Session for async S3 tests.
+    Parent fixture 's3_server' starts the mock S3 server
+    """
     FakeAioboto3Session = create_fake_session(Session, {"s3": s3_server})
 
     session = patch("aioboto3.Session", FakeAioboto3Session)
@@ -302,3 +242,6 @@ async def drs_object_s3():
     db.session.commit()
 
     yield drs_object
+
+
+pytest_plugins = ["s3_server_mock"]
---
+++ tests/constants.py
@@ -0,0 +1,13 @@
+S3_HOST = "127.0.0.1"
+S3_PORT = "9000"
+S3_SERVER_URL = f"http://{S3_HOST}:{S3_PORT}"
+S3_ACCESS_KEY = "test_access_key"
+S3_SECRET_KEY = "test_secret_key"
+
+AUTHZ_URL = "http://bento-authz.local"
+SQLALCHEMY_DATABASE_URI = "sqlite:///:memory:"
+
+DUMMY_PROJECT_ID = "b1738ea3-6ea7-4f43-a13f-51f4398818c4"
+DUMMY_DATASET_ID = "c96aa217-e07d-4d52-8c5c-df03f054fd3d"
+
+DATA_TYPE_PHENOPACKET = "phenopacket"
